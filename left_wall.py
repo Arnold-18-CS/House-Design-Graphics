a730e3f64a26eeac00c24891d67dcbf89b6ecf43
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-19 17:07:43 +0300

    Added windows to the left wall

--- left_wall.py
+++ left_wall.py
@@ -22,4 +22,33 @@ ctx.set_source_rgb(0,0,0)
 ctx.set_line_width(0.8)
 ctx.stroke()
 
+# Adding Windows
+ctx.move_to(110,160)
+ctx.line_to(110,140)
+ctx.line_to(97, 135)
+ctx.line_to(97, 155)
+ctx.close_path()
+
+ctx.move_to(93, 155)
+ctx.line_to(93, 135)
+ctx.line_to(80, 130)
+ctx.line_to(80, 150)
+ctx.close_path()
+
+ctx.move_to(110, 128)
+ctx.line_to(110, 110)
+ctx.line_to(97, 105)
+ctx.line_to(97, 123)
+ctx.close_path()
+
+ctx.move_to(93, 105)
+ctx.line_to(80, 100)
+ctx.line_to(80, 119)
+ctx.line_to(93, 123)
+ctx.close_path()
+
+ctx.stroke()
+
+
+
 surface.write_to_png('left_wall.png')
