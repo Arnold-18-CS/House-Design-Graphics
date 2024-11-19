13d74eef28fbe1f85b18913cadb1b2ecd2af1f53
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-19 16:42:53 +0300

    Basic structure of left wall

--- left_wall.py
+++ left_wall.py
@@ -1,6 +1,23 @@
 import cairo
 
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 400, 300)
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 400, 400)
 ctx = cairo.Context(surface)
-ctx.set_source_rgb(0.8,0.8,0.8)
+ctx.set_source_rgb(1,1,1)
 ctx.paint()
+
+# Wall structure
+ctx.move_to(120, 180)
+ctx.line_to(120, 95)
+ctx.line_to(70, 80)
+ctx.line_to(70, 165)
+ctx.close_path()
+
+ctx.move_to(120, 170)
+ctx.line_to(70, 155)
+ctx.line_to(70, 165)
+
+ctx.set_source_rgb(0,0,0)
+ctx.set_line_width(0.8)
+ctx.stroke()
+
+surface.write_to_png('left_wall.png')
